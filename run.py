c76bc9863decfac6666fdd8eac1c516c6ebb1965
RozWelch <rozwelch@gmail.com>
2022-09-16 10:43:48 +0000

    Create get_habits_data to get habits data from user

--- run.py
+++ run.py
@@ -18,12 +18,18 @@ SHEET = GSPREAD_CLIENT.open('habit_tracker')
 habits = SHEET.worksheet('dailyhabits')
 data = habits.get_all_values()
 
-print("Welcome to the daily good habits tracker!")
 # user inputs name and daily excercise / diet data
-name = input("Please enter your name\n")
-stretch_time = input(f"Hello {name}, please enter your minutes spent streching today: ")
-walk_dog_time = input(f"Enter your time minutes walking the dog today: ")
-gym_time = input(f"Enter your time minutes at the gym today: ")
-veg_portion = input(f"Enter how many portions of veg you ate today: ")
-glasses_water = input(f"Enter how many glasses of water your drank today: ")
-green_tea = input(f"Enter how many cups of green tea you drank (instead of the usual tea/coffee!): ")
+def get_habits_data():
+    print("Welcome to the daily good habits tracker!")
+
+    name = input("Please enter your name\n")
+    stretch_time = input(f"Hello {name}, please enter your minutes spent streching today: ")
+    walk_dog_time = input("Enter your time minutes walking the dog today: ")
+    gym_time = input("Enter your time minutes at the gym today: ")
+    veg_portion = input("Enter how many portions of veg you ate today: ")
+    glasses_water = input("Enter how many glasses of water your drank today: ")
+    green_tea = input("Enter how many cups of green tea you drank (instead of the usual tea/coffee!): ")
+
+# check if the user input data is valid
+
+get_habits_data()
